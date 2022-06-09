fc45549a2a54aae92349f959831d0b95b085fa2f
bkylee <brian.ky.lee@outlook.com>
2022-06-09 09:46:18 -0400

    Cleaned up math functions

--- script.js
+++ script.js
@@ -8,26 +8,15 @@ display.textContent = total;
 
 
 const add = function(x,y) {
-    let sum = x + y;
-    display.textConten = sum;
+    display.textContent = x+y;
   };
   
   const subtract = function(x,y) {
-      result = x-y;
-    display.textContent = result;
+    display.textContent = x-y;
   };
   
-  const sum = function(...numbers) {
-    let total = 0;
-    if (numbers.length === 0) {
-      display.textContent = total;
-    }
-    else {display.textContent total = Array.from(...numbers).reduce((x,y) => {display.textContent x + y}, 0);}
-    };
-  
-  const multiply = function(...numbers) {
-    total = 0;
-    display.textContent total = Array.from(...numbers).reduce((x,y) => {display.textContent x * y});
+  const multiply = function(x,y) {
+    display.textContent = x*y;
   };
   
   const power = function(x,y) {
