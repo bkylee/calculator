420b42c60cab19dfdb89fcd4dbcdb1ab9b1ecf3b
bkylee <brian.ky.lee@outlook.com>
2022-06-09 09:33:18 -0400

    added functions from Caclculator exercise previously completed

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const display = document.querySelector("#display");
 let total = 0; 
 display.textContent = total;
-function output(input){
-}
+
+const add = function(x,y) {
+    let sum = x + y;
+    return sum;
+  };
+  
+  const subtract = function(x,y) {
+      result = x-y;
+    return result;
+  };
+  
+  const sum = function(...numbers) {
+    let total = 0;
+    if (numbers.length === 0) {
+      return total;
+    }
+    else {return total = Array.from(...numbers).reduce((x,y) => {return x + y}, 0);}
+    };
+  
+  const multiply = function(...numbers) {
+    total = 0;
+    return total = Array.from(...numbers).reduce((x,y) => {return x * y});
+  };
+  
+  const power = function(x,y) {
+      total = x**y;
+    return total;
+  };
+  
+  const factorial = function(x) {
+    total = 0;
+      if (x === 0) {
+      return total = 1;
+    }
+      else {
+        return total = (x * factorial(x-1));
+    }
+  };
+
+  
