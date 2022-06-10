77a4adf5557303a0fe8635b1407be62fbf35ad9c
bkylee <brian.ky.lee@outlook.com>
2022-06-10 11:06:03 -0400

    Created operator function that calls the calculative functions based on operand argument

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//operate function that takes an operator and 2 nukmbers and then calls one of the functions on the numbers 
+const operate = function (operator, x,y) {
+  if (operator === "+"){
+    add(x,y);
+  }
+  else if (operator === "-") {
+    subtract(x,y);
+  }
+  else if (operator === "*") {
+    multiply(x,y);
+  }
+  else if (operator === "/") {
+    divide(x,y);
+  }
+}
+
+
 // set manipulative display variable. 
 
 const display = document.querySelector("#display"); 
@@ -17,10 +34,14 @@ const add = function(x,y) {
   const multiply = function(x,y) {
     total = x*y;
   };
+  const divide = function(x,y) {
+    total = x/y;
+  };
 
 const num1 = document.querySelector("#1");
 num1.addEventListener('onclick', updateVal(1)) 
 
 function updateVal (n) {
     currentVal = currentVal + n;
+    display.textContent = currentVal;
 }
